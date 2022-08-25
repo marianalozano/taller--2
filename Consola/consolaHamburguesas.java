05bb1e02351675476e6ec85f8dcbb6f4961cbbca
marianalozano <m.lozanor2@uniandes.edu.co>
2022-08-24 19:15:05 -0500

    consola, modelo, procesamiento

---
+++ Consola/consolaHamburguesas.java
@@ -0,0 +1,14 @@
+package uniandes.dpoo.taller2.consola;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class ConsolaHamburguesas
+{
+	
+}
